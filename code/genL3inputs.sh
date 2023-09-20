823eb176949f4f9df549900b3523e8feea0db49e
jameswyngaarden <james.wyngaarden@temple.edu>
2023-09-20 12:52:04 -0400

    fix path

--- code/genL3inputs.sh
+++ code/genL3inputs.sh
@@ -12,12 +12,12 @@ sm=5
 cope=3
 modelnum=1
 
-for task in doors; do
+for task in socialdoors doors; do
 #for task in doors; do
 	for ppi in "act"; do # "act" "VS" "dmn"; do # putting 0 first will indicate "activation"
 		for sub in `cat ${basedir}/code/newsubs.txt`; do
 	  		for run in 1; do
-	  			echo /ZPOOL/data/projects/rf1-sra-socdoors/derivatives/sub-${sub}/L1_task-${task}_model-${modelnum}_type-${ppi}_run-${run}_sm-${sm}.feat/stats/cope${cope}.nii.gz
+	  			echo /ZPOOL/data/projects/rf1-sra-socdoors/derivatives/fsl/sub-${sub}/L1_task-${task}_model-${modelnum}_type-${ppi}_run-${run}_sm-${sm}.feat/stats/cope${cope}.nii.gz
 	  		done
 	  	done
 	done
