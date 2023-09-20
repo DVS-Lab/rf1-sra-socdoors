d41028c70fe131dc42d2980251a4082e47a25ae3
jameswyngaarden <james.wyngaarden@temple.edu>
2023-09-20 13:46:21 -0400

    update L3stats scripts

--- code/L3stats.sh
+++ code/L3stats.sh
@@ -18,20 +18,23 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
 # study-specific inputs and general output folder
-N=46
+
+N=98
 copenum=$1
-copenum_thresh_randomise=99 # actual contrasts start here. no need to do randomise main effects (e.g., reward > nothing/fixation/baseline)
 copename=$2
+copenum_thresh_randomise=99 # actual contrasts start here. no need to do randomise main effects (e.g., reward > nothing/fixation/baseline)
 REPLACEME=$3 # this defines the parts of the path that differ across analyses
 TASK=$4
-#COV=$5
-MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-1_task-${TASK}_n${N}_flame1+2
+modelnum=1
+
+MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-${modelnum}_task-${TASK}_n${N}_flame1+2
 mkdir -p $MAINOUTPUT
 
 
 #### --- Two groups ------------------------------
 # set outputs and check for existing
 OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_cnum-${copenum}_cname-${copename}_twogroup
+
 if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
 
 	# run randomise if output doesn't exist and the contrasts (copes) are valid
@@ -45,41 +48,42 @@ else # try to run feat and clean up previous effort with partial output
 	# echo "re-doing: ${OUTPUT}" >> re-runL3.log
 	rm -rf ${OUTPUT}.gfeat
 
-	# create template and run FEAT analyses
-	# ITEMPLATE=${maindir}/templates/L3_template_n${N}_${TASK}_twogroup.fsf # TO DO: change to reward sensitivity covariate
-	# OTEMPLATE=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_copenum-${copenum}_twogroup.fsf
-	# sed -e 's@OUTPUT@'$OUTPUT'@g' \
-	# -e 's@COPENUM@'$copenum'@g' \
-	# -e 's@REPLACEME@'$REPLACEME'@g' \
-	# -e 's@BASEDIR@'$maindir'@g' \
-	# <$ITEMPLATE> $OTEMPLATE
-	# feat $OTEMPLATE
+	 create template and run FEAT analyses
+	 ITEMPLATE=${maindir}/templates/L3_model-${modelnum}_task-${TASK}_${REPLACEME}_n${N}.fsf # TO DO: change to reward sensitivity covariate
+	 OTEMPLATE=${MAINOUTPUT}/L3_model-${modelnum}_task-${TASK}_${REPLACEME}_copenum-${copenum}.fsf
+	 sed -e 's@OUTPUT@'$OUTPUT'@g' \
+	 -e 's@COPENUM@'$copenum'@g' \
+	 -e 's@REPLACEME@'$REPLACEME'@g' \
+	 -e 's@BASEDIR@'$maindir'@g' \
+	 -e 's@MODEL@'$modelnum'@g' \
+	 <$ITEMPLATE> $OTEMPLATE
+	 feat $OTEMPLATE
 
 	# delete unused files
 	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
 	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
 	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
-	#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
 	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
-
+	#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
+	
 fi
 
 
 ### --- Two groups with covariates ------------------------------
 # set outputs and check for existing
-OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_cnum-${copenum}_cname-${copename}_twogroup_wCovs
-if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
-
+#OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_cnum-${copenum}_cname-${copename}_twogroup_wCovs
+#if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
+#
 	# run randomise if output doesn't exist and the contrasts (copes) are valid
-	cd ${OUTPUT}.gfeat/cope1.feat
-	if [ ! -e randomise_tfce_corrp_tstat4.nii.gz ] && [ $copenum -ge $copenum_thresh_randomise ]; then
-		randomise -i filtered_func_data.nii.gz -o randomise -d design.mat -t design.con -m mask.nii.gz -T -c 2.6 -n 10000
-	fi
+#	cd ${OUTPUT}.gfeat/cope1.feat
+#	if [ ! -e randomise_tfce_corrp_tstat4.nii.gz ] && [ $copenum -ge $copenum_thresh_randomise ]; then
+#		randomise -i filtered_func_data.nii.gz -o randomise -d design.mat -t design.con -m mask.nii.gz -T -c 2.6 -n 10000
+#	fi
 
-else # try to run feat and clean up previous effort with partial output
+#else # try to run feat and clean up previous effort with partial output
 
 	# echo "re-doing: ${OUTPUT}" >> re-runL3.log
-	rm -rf ${OUTPUT}.gfeat
+#	rm -rf ${OUTPUT}.gfeat
 
 	# create template and run FEAT analyses
 	# ITEMPLATE=${maindir}/templates/L3_template_n${N}_${TASK}_twogroup_wCovs.fsf # TO DO: change to reward sensitivity covariate
@@ -92,48 +96,48 @@ else # try to run feat and clean up previous effort with partial output
 	# feat $OTEMPLATE
 
 	# delete unused files
-	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
-	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
-	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
+#	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
+#	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
+#	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
 	#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
-	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
+#	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
 
-fi
+#fi
 
 
 
 ### --- One group ------------------------------
 # set outputs and check for existing
-OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_cnum-${copenum}_cname-${copename}_onegroup_tag
-if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
-
+#OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_cnum-${copenum}_cname-${copename}_onegroup_tag
+#if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
+#
 	# run randomise if output doesn't exist and the contrasts (copes) are valid
-	cd ${OUTPUT}.gfeat/cope1.feat
-	if [ ! -e randomise_tfce_corrp_tstat2.nii.gz ] && [ $copenum -ge $copenum_thresh_randomise ]; then
-		randomise -i filtered_func_data.nii.gz -o randomise -d design.mat -t design.con -m mask.nii.gz -T -c 2.6 -n 10000
-	fi
-
-else # try to run feat and clean up previous effort with partial output
-
-	echo "re-doing: ${OUTPUT}" >> re-runL3.log
-	rm -rf ${OUTPUT}.gfeat
-
-	# create template and run FEAT analyses
-	ITEMPLATE=${maindir}/templates/L3_template_n${N}.fsf
-	OTEMPLATE=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_copenum-${copenum}.fsf
-	sed -e 's@OUTPUT@'$OUTPUT'@g' \
-	-e 's@COPENUM@'$copenum'@g' \
-	-e 's@REPLACEME@'$REPLACEME'@g' \
-	-e 's@BASEDIR@'$maindir'@g' \
-	-e 's@TASK@'$TASK'@g' \
-	<$ITEMPLATE> $OTEMPLATE
-	feat $OTEMPLATE
-
-fi
+#	cd ${OUTPUT}.gfeat/cope1.feat
+#	if [ ! -e randomise_tfce_corrp_tstat2.nii.gz ] && [ $copenum -ge $copenum_thresh_randomise ]; then
+#		randomise -i filtered_func_data.nii.gz -o randomise -d design.mat -t design.con -m mask.nii.gz -T -c 2.6 -n 10000
+#	fi
+#
+#else # try to run feat and clean up previous effort with partial output
+#
+#	echo "re-doing: ${OUTPUT}" >> re-runL3.log
+#	rm -rf ${OUTPUT}.gfeat
+#
+#	# create template and run FEAT analyses
+#	ITEMPLATE=${maindir}/templates/L3_template_n${N}.fsf
+#	OTEMPLATE=${MAINOUTPUT}/L3_task-${TASK}_${REPLACEME}_copenum-${copenum}.fsf
+#	sed -e 's@OUTPUT@'$OUTPUT'@g' \
+#	-e 's@COPENUM@'$copenum'@g' \
+#	-e 's@REPLACEME@'$REPLACEME'@g' \
+#	-e 's@BASEDIR@'$maindir'@g' \
+#	-e 's@TASK@'$TASK'@g' \
+#	<$ITEMPLATE> $OTEMPLATE
+#	feat $OTEMPLATE
+#
+#fi
 
 # delete unused files
-rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
-rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
-rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
+#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
+#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
+#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
 #rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
-rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
+#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
--- code/run_L3stats.sh
+++ code/run_L3stats.sh
@@ -10,45 +10,30 @@ maindir="$(dirname "$scriptdir")"
 
 
 # this loop defines the different types of analyses that will go into the group comparisons
-for TASK in doors socialdoors; do
-#for COV in alcohol tobacco marijuana; do
-for analysis in ppi_seed-VS_thr5 nppi-dmn nppi-ecn; do # act ppi_seed-NAcc-act_n46 ppi_seed-vmpfc nppi-dmn nppi-ecn ppi_seed | type-${type}_run-01
-	analysistype=type-${analysis}_run-1
+for TASK in socialdoors; do
+	for analysis in act; do #ppi_seed-VS_thr5 nppi-dmn nppi-ecn; do # act ppi_seed-NAcc-act_n46 ppi_seed-vmpfc nppi-dmn nppi-ecn ppi_seed | type-${type}_run-01
+		analysistype=type-${analysis}
 
-	# these define the cope number (copenum) and cope name (copename)
-	# for copeinfo in "1 C_pun" "2 C_rew" "3 F_pun" "4 F_rew" "5 S_pun" "6 S_rew"; do
-	for copeinfo in "4 win-loss"; do #"1 win" "2 loss" "3 decision"; do #"3 dec" "4 win-loss" "5 phys"; do
+		# these define the cope number (copenum) and cope name (copename)
+		for copeinfo in "4 win-loss"; do #"1 win" "2 loss" "3 decision"; do #"3 dec" "4 win-loss" "5 phys"; do
 
-		# split copeinfo variable
-		set -- $copeinfo
-		copenum=$1
-		copename=$2
+			# split copeinfo variable
+			set -- $copeinfo
+			copenum=$1
+			copename=$2
 
-		if [ "${analysistype}" == "type-act" ] && [ "${copeinfo}" == "5 phys" ]; then
-			echo "skipping phys for activation since it does not exist..."
-			continue
-		fi
+			if [ "${analysistype}" == "type-act" ] && [ "${copeinfo}" == "5 phys" ]; then
+				echo "skipping phys for activation since it does not exist..."
+				continue
+			fi
 
-		# rename copeinfo variable for activation and ppi analyses
-		if [ "${analysistype}" == "type-act" ] && [ "${copeinfo}" == "24 F-SC_rew-pun" ]; then
-			copenum=23
-			copename=F-SC_rew-pun
-		elif [ "${analysistype}" == "type-ppi_seed-NAcc" ] && [ "${copeinfo}" == "24 F-SC_rew-pun" ]; then
-			copenum=23
-			copename=F-SC_rew-pun
-		elif [ "${analysistype}" == "type-ppi_seed-NAcc" ] && [ "${copeinfo}" == "23 phys" ]; then
-			copenum=24
-			copename=phys
-		fi
+			NCORES=15
+			SCRIPTNAME=${maindir}/code/L3stats.sh
+			while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+				sleep 1s
+			done
+			bash $SCRIPTNAME $copenum $copename $analysistype $TASK &
 
-		NCORES=15
-		SCRIPTNAME=${maindir}/code/L3stats.sh
-		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-			sleep 1s
 		done
-		bash $SCRIPTNAME $copenum $copename $analysistype $TASK $COV &
-
 	done
-done
-#done
 done
