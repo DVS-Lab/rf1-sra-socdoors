f20e8a51d64b877a3e39d974e48bbca4e4919fe6
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-27 10:53:51 -0400

    run new subs to add to SfN

--- code/gen3colfiles.sh
+++ code/gen3colfiles.sh
@@ -14,7 +14,7 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
-baseout=/data/projects/rf1-sra-socdoors/derivatives/fsl/EVFiles
+baseout=/ZPOOL/data/projects/rf1-sra-socdoors/derivatives/fsl/EVFiles
 if [ ! -d ${baseout} ]; then
   mkdir -p $baseout
 fi
@@ -23,7 +23,7 @@ sub=$1
 
 for task in doors socialdoors; do
 	for run in 1; do
-  			input=/data/projects/rf1-sra/stimuli/Scan-Social_Doors/data/${sub}/sub-${sub}_task-${task}_run-${run}_events.tsv
+  			input=/ZPOOL/data/projects/rf1-sra/stimuli/Scan-Social_Doors/data/${sub}/sub-${sub}_task-${task}_run-${run}_events.tsv
   			output=${baseout}/sub-${sub}/${task}
 			mkdir -p $output
   			if [ -e $input ]; then
