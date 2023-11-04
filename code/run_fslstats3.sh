4a64a79ad286625f0575c0dc90eeb26af866effe
jameswyngaarden <james.wyngaarden@temple.edu>
2023-11-04 17:24:41 -0400

    fix script for fsl

--- code/run_fslstats3.sh
+++ code/run_fslstats3.sh
@@ -6,23 +6,23 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-location=fsl
+location=randomise
 
-for ana in "age" "agexnbs" "agexoafem"; do
+for ana in "age" "agexnbs" "agexoafem" "ageXnbs" "ageXoafem"; do
   for type in "act" "nppi-dmn" "ppi_seed-VS"; do
     for img in "clustere"; do
       for copeinfo in "4 win-loss"; do
-        
+
         set -- $copeinfo
         cnum=$1
         cname=$2
-        
+
         count=1
-        if [ "$location" == "randomise" ] then
+        if [ "$location" == "randomise" ]; then
           for tfile in ${maindir}/derivatives/randomise/L3_model-1_task-socialdoors_type-${type}_cnum-${cnum}_cname-${cname}_${ana}/_${img}_corrp_tstat*.nii.gz; do
             echo "Type-${type}_cnum-${cnum}_cname-${cname}_${ana}/_${img}_corrp_tstat${count}_tstat.nii.gz"
             var=$(fslstats ${tfile} -R)
-          
+
             yvar=$(echo ${var} | awk '{print $2}')
             echo "Max 1-p = $yvar"
 
@@ -33,10 +33,24 @@ for ana in "age" "agexnbs" "agexoafem"; do
             fi
             count=$(($count+1))
           done
-      elif [ "$location" == "fsl" ]; then
-        
-      fi
-      
+      	elif [ "$location" ==  "fsl" ]; then
+           for tfile in ${maindir}/derivatives/fsl/L3_model-1_task-socialdoors_n58_flame1+2/L3_task-socialdoors_type-${type}_cnum-${cnum}_cname-${cname}_flame1+2_single-group-average_${ana}.gfeat/cope1.feat/thresh_zstat*.nii.gz; do
+            #echo "Type-${type}_cnum-${cnum}_cname-${cname}/thresh_zstat${count}.nii.gz"
+            var=$(fslstats ${tfile} -R)
+
+            yvar=$(echo ${var} | awk '{print $2}')
+            #echo "Thresh zstat${count} max intensity = $yvar"
+
+            if (( $(echo "$yvar > 0" |bc -l) )); then
+              #ofile=${maindir}/derivatives/randomise/L3_model-1_task-socialdoors_type-${type}_cnum-${cnum}_cname-${cname}_${ana}/_thresh_corrp_tstat${count}.nii.gz
+              echo "See significant cluster(s) in ${tfile}"
+              #fslmaths ${tfile} -thr .95 ${ofile}
+            fi
+            count=$(($count+1))
+          done
+      	else
+	  echo "Enter proper var for location"
+	fi
       done
     done
   done
