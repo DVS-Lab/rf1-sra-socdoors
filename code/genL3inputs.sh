b436f2b8cb284c05b07f80bf3a933a47ccdcffc4
jameswyngaarden <james.wyngaarden@temple.edu>
2024-09-23 20:52:10 -0400

    prep L3 model 2s

--- code/genL3inputs.sh
+++ code/genL3inputs.sh
@@ -10,16 +10,16 @@ basedir="$(dirname "$scriptdir")"
 sm=5
 
 # Set cope & model nums
-cope=5
+cope=4
 modelnum=1
 
 #for task in socialdoors doors; do
-#for task in socialdoors; do
 for task in socialdoors; do
-	for ppi in "ppi_seed-VS"; do
-	#for ppi in "nppi-dmn"; do
+#for task in doors; do
+	#for ppi in "ppi_seed-VS"; do
+	for ppi in "nppi-dmn"; do
 	#for ppi in "act"; do
-		for sub in `cat ${basedir}/code/sublist_all.txt`; do
+		for sub in `cat ${basedir}/code/sublist_model-2.txt`; do
 	  		for run in 1; do
 				# For comparisons between two groups:
 	  			echo /ZPOOL/data/projects/rf1-sra-socdoors/derivatives/fsl/sub-${sub}/L1_task-${task}_model-${modelnum}_type-${ppi}_run-${run}_sm-${sm}.feat/stats/cope${cope}.nii.gz
